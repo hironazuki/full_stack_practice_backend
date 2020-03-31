eb45eaa4c2df49df83cb3402e13b649be64fb101
hironakazuki <far_east_nightbird30@icloud.com>
2020-03-31 16:35:59 +0900

    uibuild

---
+++ build/precache-manifest.da6c8b550cff5313f6a9bac4d2d9e7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc61e82197c332c49315c50ba5e1fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ed03a8c4818bcaae11a",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "f86840ab33580cecf5b0",
+    "url": "/static/js/2.d509af12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d509af12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed03a8c4818bcaae11a",
+    "url": "/static/js/main.20bdab4a.chunk.js"
+  },
+  {
+    "revision": "7cbb070ae2e349df2854",
+    "url": "/static/js/runtime-main.a161d986.js"
+  }
+]);
