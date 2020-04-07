6941c159574caa1a0ae0bf9a583d90aa19457409
hironakazuki <far_east_nightbird30@icloud.com>
2020-04-07 21:42:14 +0900

    learn 4b practice

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,25 +2,21 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 
-notesRouter.get('/', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes.map(note => note.toJSON()))
-  })
+notesRouter.get('/', async (req, res) => {
+  const notes = await Note.find({})
+  res.json(notes.map(note => note.toJSON()))
 })
 
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then(note => {
-      if (note) {
-        res.json(note.toJSON())
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (req, res, next) => {
+  const note = await Note.findById(req.params.id)
+  if (note) {
+    res.json(note.toJSON())
+  } else {
+    res.status(404).end()
+  }
 })
 
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res, next) => {
   const body = req.body
 
   const note = new Note({
@@ -29,31 +25,14 @@ notesRouter.post('/', (req, res, next) => {
     date: new Date(),
   })
 
-  note
-    .save()
-    .then(savedNote => savedNote.toJSON())
-    .then(savedAndFormattedNote => {
-      res.json(savedAndFormattedNote)
-    })
-    .catch(error => next(error))
+  const saveNote = await note.save()
+  res.json(saveNote.toJSON())
 })
 
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (req, res, next) => {
+  await Note.findByIdAndRemove(req.params.id)
+  res.status(204).end()
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-
 
 notesRouter.put('/:id', (req, res, next) => {
   const body = req.body
