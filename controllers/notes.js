d80310c17c326fa733aaa2b797b9d3ea12ef3f6d
hironakazuki <far_east_nightbird30@icloud.com>
2020-04-09 17:03:52 +0900

    learn 4d practice

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note
@@ -18,10 +19,23 @@ notesRouter.get('/:id', async (req, res, next) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 notesRouter.post('/', async (req, res, next) => {
   const body = req.body
+  const token = getTokenFrom(req)
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
